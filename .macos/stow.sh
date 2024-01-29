b9acc97746f4d37f898609e4dd452691c0f97222
Simon Krenn <simonkrenn@Simons-MBP.home>
2024-01-29 19:58:05 +0100

    tmux config

--- .macos/stow.sh
+++ .macos/stow.sh
@@ -1,3 +1,4 @@
 stow -v -d $PWD/.config -t $HOME tmux
 stow -v -d $PWD/.config -t $HOME/.config/fish/ fish
 stow -v -d $PWD/.config -t $HOME/.config/alacritty/ alacritty
+stow -v -d $PWD/.config -t $HOME/.config/tmux/ tmux
