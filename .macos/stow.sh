14cacbd88fd5b30fb4caf866c034f85a17a10867
Simon Krenn <simon.krenn@teamviewer.com>
2024-02-15 09:44:55 +0100

    joshuto conf

--- .macos/stow.sh
+++ .macos/stow.sh
@@ -3,3 +3,4 @@ stow -v -d $PWD/.config -t $HOME/.config/fish/ fish
 stow -v -d $PWD/.config -t $HOME/.config/alacritty/ alacritty
 stow -v -d $PWD/.config -t $HOME/.config/nvim/ nvim
 stow -v -d $PWD/.config -t $HOME/.config/tmux/ tmux
+stow -v -d $PWD/.config -t $HOME/.config/joshuto joshuto
