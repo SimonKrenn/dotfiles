bc805d19b2ff50e2e3e9acd6c7b67f384fd86083
Simon Krenn <simon.krenn@teamviewer.com>
2024-02-23 10:20:35 +0100

    fix config

--- .macos/.config/fish/config.fish
+++ .macos/.config/fish/config.fish
@@ -1,12 +1,20 @@
 fish_add_path /opt/homebrew/bin
 starship init fish | source
-
-
-# Aliases
+zoxide init fish | source
+# global Aliases
 if [ -f $HOME/.config/fish/alias.fish ]
-    source $HOME/.config/fish//alias.fish
+	source $HOME/.config/fish//alias.fish
+end
+
+# work aliases 
+if [ -f $HOME/.config/fish/private-alias.fish ]
+	source $HOME/.config/fish/private-alias.fish
 end
 
 # bun
-set --export BUN_INSTALL "$HOME/.bun"
-set --export PATH $BUN_INSTALL/bin $PATH
+# set --export BUN_INSTALL "$HOME/.bun"
+# set --export PATH $BUN_INSTALL/bin $PATH
+
+
+# env vars
+export EDITOR=nvim
