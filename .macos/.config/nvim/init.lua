0ee0bab33e36c1124243886d6f67ebea3d490e2c
Simon Krenn <simonkrenn@Simons-MBP.home>
2024-03-30 18:46:57 +0100

    nvim: update colorscheme

--- .macos/.config/nvim/init.lua
+++ .macos/.config/nvim/init.lua
@@ -7,4 +7,5 @@ require("mason-lspconfig").setup()
 require("mini.pairs").setup()
 require("simon.settings")
 require("simon.keymap")
-require("simon.bubble-line")
+-- require("simon.bubble-line")
+-- require("/Users/simonkrenn/workspace/themes/lush-template/lua/lua-line/theme.lua");
--- .macos/.config/nvim/lua/plugins/lualine.lua
+++ .macos/.config/nvim/lua/plugins/lualine.lua
@@ -8,5 +8,5 @@ return {
 	},
 	extensions = {
 		'neo-tree'
-	}
+	},
 }
