5e1e32fcf1a54715eb11301df59d83dff851fecc
SimonKrenn <skrenn99@gmail.com>
2024-07-07 20:06:37 +0200

    feat(nvim): update colortheme

--- .config/nvim/lua/plugins/colortheme.lua
+++ .config/nvim/lua/plugins/colortheme.lua
@@ -1,42 +1,85 @@
 return {
-	"rebelot/kanagawa.nvim",
+	'marko-cerovac/material.nvim',
 	config = function()
-		-- load the colorscheme here
-		vim.cmd([[colorscheme kanagawa]])
-	end
-	-- 	-- oxocarbon theme
-	-- 	dir = 'SimonKrenn/one-cyber.nvim',
-	-- 	-- 'nyoom-engineering/oxocarbon.nvim',
-	-- 	config = function()
-	-- 		-- local oxocarbon = require("oxocarbon")
-	-- 		vim.o.termguicolors = true
-	-- 		vim.o.background = "dark"
-	-- 		vim.cmd [[colorscheme one-cyber]]
-	--
-	-- 		-- vim.api.nvim_set_hl(0, "TelescopeBorder", { fg = oxocarbon.blend, bg = oxocarbon.blend })
-	-- 		-- vim.api.nvim_set_hl(0, "TelescopePromptBorder", { fg = oxocarbon.base02, bg = oxocarbon.base02 })
-	-- 		-- vim.api.nvim_set_hl(0, "TelescopePromptNormal", { fg = oxocarbon.base05, bg = oxocarbon.base02 })
-	-- 		-- vim.api.nvim_set_hl(0, "TelescopePromptPrefix", { fg = oxocarbon.base08, bg = oxocarbon.base02 })
-	-- 		-- vim.api.nvim_set_hl(0, "TelescopeNormal", { fg = oxocarbon.none, bg = oxocarbon.blend })
-	-- 		-- vim.api.nvim_set_hl(0, "TelescopePreviewTitle", { fg = oxocarbon.base02, bg = oxocarbon.base12 })
-	-- 		-- vim.api.nvim_set_hl(0, "TelescopePromptTitle", { fg = oxocarbon.base02, bg = oxocarbon.base11 })
-	-- 		-- vim.api.nvim_set_hl(0, "TelescopeResultsTitle", { fg = oxocarbon.blend, bg = oxocarbon.blend })
-	-- 		-- vim.api.nvim_set_hl(0, "TelescopeSelection", { fg = oxocarbon.none, bg = oxocarbon.base02 })
-	-- 		-- vim.api.nvim_set_hl(0, "TelescopePreviewLine", { fg = oxocarbon.none, bg = oxocarbon.base01 })
-	-- 		-- vim.api.nvim_set_hl(0, "NeoTreeFloatBorder", { fg = oxocarbon.base13, bg = oxocarbon.base02 })
-	-- 		-- vim.api.nvim_set_hl(0, "NeoTreeNormal", { fg = oxocarbon.base13, bg = oxocarbon.base02 })
-	-- 	end
-	-- }
+		require('material').setup({
+			contrast = {
+				terminal = false, -- Enable contrast for the built-in terminal
+				sidebars = false, -- Enable contrast for sidebar-like windows ( for example Nvim-Tree )
+				floating_windows = false, -- Enable contrast for floating windows
+				cursor_line = false, -- Enable darker background for the cursor line
+				lsp_virtual_text = false, -- Enable contrasted background for lsp virtual text
+				non_current_windows = false, -- Enable contrasted background for non-current windows
+				filetypes = {},  -- Specify which filetypes get the contrasted (darker) background
+			},
 
-	-- return {
-	-- 	"SimonKrenn/one-cyber",
-	-- 	dependencies = { "rktjmp/lush.nvim" },
-	-- 	name = "one-cyber",
-	-- 	branch = "main",
-	-- 	priority = 1000,
-	-- 	config = function()
-	-- 		vim.o.background = "dark"
-	-- 		vim.o.termguicolors = true
-	-- 		vim.cmd("colorscheme one-cyber")
-	-- 	end
+			styles = { -- Give comments style such as bold, italic, underline etc.
+				comments = { --[[ italic = true ]] },
+				strings = { --[[ bold = true ]] },
+				keywords = { --[[ underline = true ]] },
+				functions = { --[[ bold = true, undercurl = true ]] },
+				variables = {},
+				operators = {},
+				types = {},
+			},
+
+			plugins = { -- Uncomment the plugins that you use to highlight them
+				-- Available plugins:
+				"coc",
+				-- "colorful-winsep",
+				-- "dap",
+				-- "dashboard",
+				-- "eyeliner",
+				-- "fidget",
+				-- "flash",
+				"gitsigns",
+				-- "harpoon",
+				-- "hop",
+				-- "illuminate",
+				"indent-blankline",
+				-- "lspsaga",
+				-- "mini",
+				-- "neogit",
+				-- "neotest",
+				"neo-tree",
+				-- "neorg",
+				"noice",
+				"nvim-cmp",
+				-- "nvim-navic",
+				-- "nvim-tree",
+				-- "nvim-web-devicons",
+				-- "rainbow-delimiters",
+				-- "sneak",
+				"telescope",
+				"trouble",
+				-- "which-key",
+				"nvim-notify",
+			},
+
+			disable = {
+				colored_cursor = false, -- Disable the colored cursor
+				borders = false, -- Disable borders between vertically split windows
+				background = false, -- Prevent the theme from setting the background (NeoVim then uses your terminal background)
+				term_colors = false, -- Prevent the theme from setting terminal colors
+				eob_lines = true -- Hide the end-of-buffer lines
+			},
+
+			high_visibility = {
+				lighter = false, -- Enable higher contrast text for lighter style
+				darker = false -- Enable higher contrast text for darker style
+			},
+
+			lualine_style = 'stealth', -- Lualine style ( can be 'stealth' or 'default' )
+
+			async_loading = true, -- Load parts of the theme asynchronously for faster startup (turned on by default)
+
+			custom_colors = nil, -- If you want to override the default colors, set this to a function
+
+			custom_highlights = {}, -- Overwrite highlights with your own
+		})
+	end,
+	init = function()
+		vim.g.material_style = 'deep ocean'
+		vim.cmd('colorscheme material')
+	end,
+
 }
