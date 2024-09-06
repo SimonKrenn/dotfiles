7c4cd1df6b8e28b09fc82ea1647760042b71097f
Simon Krenn <simon.krenn@teamviewer.com>
2024-09-06 13:06:11 +0200

    macos defaults

--- .macos/os-defaults.sh
+++ .macos/os-defaults.sh
@@ -4,6 +4,8 @@ defaults write com.apple.finder ShowPathbar -bool true
 
 defaults write com.apple.finder ShowStatusBar -bool true
 
+defaults write -g ApplePressAndHoldEnabled -bool true
+
 defaults write com.apple.finder "FXPreferredViewStyle" -string "clmv" 
 
 defaults write com.apple.finder "AppleShowAllFiles" -bool "false"
