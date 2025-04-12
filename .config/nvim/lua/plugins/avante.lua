56c1d38b36aa632881bf82d30883fb01006c37ac
Simon Krenn <skrenn99@gmail.com>
2025-04-12 11:24:10 +0200

    feat(nvim): add avante for agentic AI

---
+++ .config/nvim/lua/plugins/avante.lua
@@ -0,0 +1,14 @@
+return {
+	"yetone/avante.nvim",
+	event = "VeryLazy",
+	lazy = false,
+	version = false,
+	opts = {
+		provider = "ollama",
+		ollama = {
+			endpoint = "http://127.0.0.1:11434",
+			model = "gemma3:27b"
+		}
+	},
+	build = "make",
+}
