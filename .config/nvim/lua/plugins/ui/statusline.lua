5bce99d710d2ef05015d698fef94517eb25c5d08
Simon Krenn <skrenn99@gmail.com>
2025-08-16 08:42:50 +0200

    feat(nvim): ui related plugins

--- .config/nvim/lua/plugins/ui/statusline.lua
+++ .config/nvim/lua/plugins/ui/statusline.lua
@@ -2,13 +2,16 @@ return {
   'nvim-lualine/lualine.nvim',
   enabled = true,
   opts = {
-    theme = 'auto',
-    icons_enabled = true,
-    component_separators = '|',
-    section_separators = '|',
+    options = {
+      theme = 'auto',
+      icons_enabled = true,
+      component_separators = '',
+      section_separators = '',
+    },
     sections = {
-      lualine_a = {"branch", 'diff'},
+      lualine_a = {"branch"},
       lualine_b = {"filename"},
+      lualine_c = {"diff"},
       lualine_x = {},
       lualine_y = {},
       lualine_z = {'mode'},
