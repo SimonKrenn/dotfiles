df76092feacad4e3f9ac8abe9a91fe1a0a59424e
Simon Krenn <skrenn99@gmail.com>
2025-08-16 08:42:33 +0200

    feat(nvim): init obsidian

---
+++ .config/nvim/lua/plugins/lang/obsidian.lua
@@ -0,0 +1,18 @@
+return {
+    'obsidian-nvim/obsidian.nvim',
+    version = '*',
+    lazy = 'true',
+    ft = 'markdown'
+    opts = {
+        workspaces = {
+          {
+            name = 'personal',
+            path = '~/vaults/personal',
+          },
+          {
+            name = 'work',
+            paht = '~/vaults/work'
+          }
+        },
+    }
+}
