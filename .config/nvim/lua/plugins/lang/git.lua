006500413a642f169ab236b505d5112ee664f98c
Simon Krenn <skrenn99@gmail.com>
2025-08-18 07:57:27 +0200

    feat(nvim): lang specific setup

---
+++ .config/nvim/lua/plugins/lang/git.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "lewis6991/gitsigns.nvim",
+    event = "VeryLazy",
+    opts = {
+      current_line_blame = true,
+      current_line_blame_opts = {
+        delay = 50,
+        ignore_whitespace = true,
+      },
+    },
+  },
+  {
+    "sindrets/diffview.nvim",
+  },
+}
--- .config/nvim/lua/plugins/lang/obsidian.lua
+++ .config/nvim/lua/plugins/lang/obsidian.lua
@@ -1,21 +1,22 @@
 return {
-    'obsidian-nvim/obsidian.nvim',
-    version = '*',
-    lazy = 'true',
-    ft = 'markdown',
-    opts = {
-        workspaces = {
-          {
-            name = 'personal',
-            path = '~/vaults/personal',
-          },
-          {
-            name = 'work',
-            paht = '~/vaults/work',
-          },
-        },
-    completion = {
-        blink = true
+  "obsidian-nvim/obsidian.nvim",
+  requires = { "nvim-lua/plenary.nvim" },
+  version = "*",
+  lazy = "true",
+  ft = "markdown",
+  opts = {
+    workspaces = {
+      {
+        name = "personal",
+        path = "~/vaults/personal",
       },
-    }
+      {
+        name = "work",
+        paht = "~/vaults/work",
+      },
+    },
+    completion = {
+      blink = true,
+    },
+  },
 }
