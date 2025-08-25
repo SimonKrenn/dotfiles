7acdcda81a38e51c149b2828fb407d28abad86e1
Simon Krenn <skrenn99@gmail.com>
2025-08-25 10:47:48 +0200

    chore(nvim): update flash keys

--- .config/nvim/lua/plugins/editor/flash.lua
+++ .config/nvim/lua/plugins/editor/flash.lua
@@ -3,7 +3,7 @@ return {
   opts = {},
   keys = {
     {
-      "s",
+      "<leader>ss",
       mode = { "n", "x", "o" },
       function()
         require("flash").jump()
@@ -11,7 +11,7 @@ return {
       desc = "Flash",
     },
     {
-      "S",
+      "<leader>sS",
       mode = { "n", "x", "o" },
       function()
         require("flash").treesitter()
