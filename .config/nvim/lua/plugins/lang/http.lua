2c708f03897400d4dceb81cb8ceb2dba702948c1
Simon Krenn <skrenn99@gmail.com>
2025-09-23 15:05:27 +0200

    feat: add kulala

---
+++ .config/nvim/lua/plugins/lang/http.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "mistweaverco/kulala.nvim",
+    ft = { "http", "rest" },
+    opts = {
+      global_keymaps = true,
+      global_keymaps_prefix = "<leader>K",
+      kulala_keymaps_prefix = "",
+    },
+  },
+}
