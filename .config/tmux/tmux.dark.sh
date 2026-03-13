28b889923ccb09c4d80f26121b4bb6a6ca975c1f
Simon Krenn <skrenn99@gmail.com>
2026-03-13 19:43:01 +0100

    feat(tmux): switch to tmux-dotbar for statusline

--- .config/tmux/tmux.dark.sh
+++ .config/tmux/tmux.dark.sh
@@ -1,4 +1,8 @@
 echo "switching dark"
 
-set -g @catppuccin_flavor 'macchiato'
-tmux source ~/.tmux.conf
+tmux set-option -g @tmux-dotbar-bg '#191d21'
+tmux set-option -g @tmux-dotbar-fg '#45505b'
+tmux set-option -g @tmux-dotbar-fg-current '#E0E0E0'
+tmux set-option -g @tmux-dotbar-fg-session '#888888'
+tmux set-option -g @tmux-dotbar-fg-prefix '#F4457D'
+tmux run-shell ~/.tmux/plugins/tmux-dotbar/dotbar.tmux
--- .config/tmux/tmux.light.sh
+++ .config/tmux/tmux.light.sh
@@ -1,5 +1,9 @@
 echo "switching light"
 
-set -g @catppuccin_flavor 'latte' 
+tmux set-option -g @tmux-dotbar-bg '#ffffff'
+tmux set-option -g @tmux-dotbar-fg '#c2c3c5'
+tmux set-option -g @tmux-dotbar-fg-current '#313131'
+tmux set-option -g @tmux-dotbar-fg-session '#848484'
+tmux set-option -g @tmux-dotbar-fg-prefix '#E3329F'
 
-tmux source ~/.tmux.conf
+tmux run-shell ~/.tmux/plugins/tmux-dotbar/dotbar.tmux
