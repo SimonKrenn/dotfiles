db60b0cf269e0054d2a04bb24290fb53a84a3bda
Simon Krenn <skrenn99@gmail.com>
2026-04-19 12:19:46 +0200

    feat(nvim): add global keymap list for which-key

--- .config/nvim/lua/plugins/utils/which-key.lua
+++ .config/nvim/lua/plugins/utils/which-key.lua
@@ -1,7 +1,16 @@
 return {
-  'folke/which-key.nvim',
-  event = 'VeryLazy',
+  "folke/which-key.nvim",
+  event = "VeryLazy",
   opts = {
-    preset = 'helix'
+    preset = "helix",
+  },
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = true })
+      end,
+      desc = "Buffer Local Keymaps (which-key)",
+    },
   },
 }
