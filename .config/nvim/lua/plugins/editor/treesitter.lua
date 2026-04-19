13eda6c1bdfbfea7418599fbf77ad01419ddd160
Simon Krenn <skrenn99@gmail.com>
2026-04-19 12:05:19 +0200

    chore: upgrade treesitter

--- .config/nvim/lua/plugins/editor/treesitter.lua
+++ .config/nvim/lua/plugins/editor/treesitter.lua
@@ -1,3 +1,72 @@
+local parsers = {
+  "lua",
+  "typescript",
+  "vimdoc",
+  "html",
+  "java",
+  "yaml",
+  "jsdoc",
+  "javascript",
+  "json",
+  "toml",
+  "bash",
+  "fish",
+  "markdown_inline",
+  "markdown",
+  "regex",
+  "tsx",
+  "css",
+  "nix",
+  "git_config",
+  "gitcommit",
+  "git_rebase",
+  "gitignore",
+  "gitattributes",
+  "go",
+  "gomod",
+  "gowork",
+  "cpp",
+  "rust",
+  "zig",
+}
+
+local function ensure_treesitter_runtime_in_rtp()
+  local ok_lazy, lazy_config = pcall(require, "lazy.core.config")
+  if not ok_lazy then
+    return
+  end
+
+  local plugin = lazy_config.plugins["nvim-treesitter"]
+  local runtime_dir = plugin and (plugin.dir .. "/runtime") or nil
+  if not runtime_dir or not vim.uv.fs_stat(runtime_dir) then
+    return
+  end
+
+  local rtp = vim.opt.runtimepath:get()
+  if not vim.tbl_contains(rtp, runtime_dir) then
+    vim.opt.runtimepath:append(runtime_dir)
+  end
+end
+
+local function attach_treesitter(bufnr)
+  if not vim.api.nvim_buf_is_loaded(bufnr) then
+    return
+  end
+
+  if vim.bo[bufnr].buftype ~= "" or vim.bo[bufnr].filetype == "" then
+    return
+  end
+
+  local ok_start = pcall(vim.treesitter.start, bufnr)
+  if not ok_start then
+    return
+  end
+
+  vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+  vim.wo[0][0].foldmethod = "expr"
+  vim.bo[bufnr].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+end
+
 return {
   {
     "nvim-treesitter/nvim-treesitter",
@@ -5,68 +74,43 @@ return {
     lazy = false,
     build = ":TSUpdate",
     config = function()
-      local parsers = {
-        "lua",
-        "typescript",
-        "vimdoc",
-        "html",
-        "java",
-        "yaml",
-        "jsdoc",
-        "javascript",
-        "json",
-        "jsonc",
-        "toml",
-        "bash",
-        "fish",
-        "markdown_inline",
-        "markdown",
-        "regex",
-        "tsx",
-        "css",
-        "nix",
-        "git_config",
-        "gitcommit",
-        "git_rebase",
-        "gitignore",
-        "gitattributes",
-        "go",
-        "gomod",
-        "gowork",
-        "cpp",
-        "rust",
-        "zig",
-      }
+      ensure_treesitter_runtime_in_rtp()
+      local ts = require("nvim-treesitter")
 
-      require("nvim-treesitter").setup({})
-      require("nvim-treesitter").install(parsers)
+      ts.setup({})
+      ts.install(parsers)
+
+      vim.treesitter.language.register("tsx", "typescriptreact")
+      vim.treesitter.language.register("jsx", "javascriptreact")
 
       local group = vim.api.nvim_create_augroup("treesitter-main", { clear = true })
       vim.api.nvim_create_autocmd("FileType", {
         group = group,
-        pattern = "*",
         callback = function(args)
-          local ok = pcall(vim.treesitter.start, args.buf)
-          if not ok then
-            return
-          end
-
-          vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
-          vim.wo[0][0].foldmethod = "expr"
-          vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+          attach_treesitter(args.buf)
         end,
       })
+
+      vim.schedule(function()
+        for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+          attach_treesitter(buf)
+        end
+      end)
     end,
   },
   {
     "nvim-treesitter/nvim-treesitter-textobjects",
     branch = "main",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+    },
     config = function()
-      local select = require("nvim-treesitter-textobjects.select")
-      local move = require("nvim-treesitter-textobjects.move")
-      local swap = require("nvim-treesitter-textobjects.swap")
+      local ok_textobjects, textobjects = pcall(require, "nvim-treesitter-textobjects")
+      if not ok_textobjects or not textobjects.setup then
+        return
+      end
 
-      require("nvim-treesitter-textobjects").setup({
+      textobjects.setup({
         select = {
           lookahead = true,
         },
@@ -75,49 +119,44 @@ return {
         },
       })
 
-      vim.keymap.set({ "x", "o" }, "aa", function()
-        select.select_textobject("@parameter.outer", "textobjects")
-      end)
-      vim.keymap.set({ "x", "o" }, "ia", function()
-        select.select_textobject("@parameter.inner", "textobjects")
-      end)
-      vim.keymap.set({ "x", "o" }, "af", function()
-        select.select_textobject("@function.outer", "textobjects")
-      end)
-      vim.keymap.set({ "x", "o" }, "if", function()
-        select.select_textobject("@function.inner", "textobjects")
-      end)
-      vim.keymap.set({ "x", "o" }, "ac", function()
-        select.select_textobject("@class.outer", "textobjects")
-      end)
-      vim.keymap.set({ "x", "o" }, "ic", function()
-        select.select_textobject("@class.inner", "textobjects")
-      end)
+      local ok_select, select = pcall(require, "nvim-treesitter-textobjects.select")
+      local ok_move, move = pcall(require, "nvim-treesitter-textobjects.move")
+      local ok_swap, swap = pcall(require, "nvim-treesitter-textobjects.swap")
+      if not (ok_select and ok_move and ok_swap) then
+        return
+      end
 
-      vim.keymap.set({ "n", "x", "o" }, ")f", function()
-        move.goto_next_start("@function.outer", "textobjects")
-      end)
-      vim.keymap.set({ "n", "x", "o" }, ")c", function()
-        move.goto_next_start("@class.outer", "textobjects")
-      end)
-      vim.keymap.set({ "n", "x", "o" }, ")F", function()
-        move.goto_next_end("@function.outer", "textobjects")
-      end)
-      vim.keymap.set({ "n", "x", "o" }, ")C", function()
-        move.goto_next_end("@class.outer", "textobjects")
-      end)
-      vim.keymap.set({ "n", "x", "o" }, "(f", function()
-        move.goto_previous_start("@function.outer", "textobjects")
-      end)
-      vim.keymap.set({ "n", "x", "o" }, "(c", function()
-        move.goto_previous_start("@class.outer", "textobjects")
-      end)
-      vim.keymap.set({ "n", "x", "o" }, "(F", function()
-        move.goto_previous_end("@function.outer", "textobjects")
-      end)
-      vim.keymap.set({ "n", "x", "o" }, "(C", function()
-        move.goto_previous_end("@class.outer", "textobjects")
-      end)
+      local select_maps = {
+        aa = "@parameter.outer",
+        ia = "@parameter.inner",
+        af = "@function.outer",
+        ["if"] = "@function.inner",
+        ac = "@class.outer",
+        ic = "@class.inner",
+      }
+
+      for lhs, query in pairs(select_maps) do
+        vim.keymap.set({ "x", "o" }, lhs, function()
+          select.select_textobject(query, "textobjects")
+        end)
+      end
+
+      local move_maps = {
+        [")f"] = { move.goto_next_start, "@function.outer" },
+        [")c"] = { move.goto_next_start, "@class.outer" },
+        [")F"] = { move.goto_next_end, "@function.outer" },
+        [")C"] = { move.goto_next_end, "@class.outer" },
+        ["(f"] = { move.goto_previous_start, "@function.outer" },
+        ["(c"] = { move.goto_previous_start, "@class.outer" },
+        ["(F"] = { move.goto_previous_end, "@function.outer" },
+        ["(C"] = { move.goto_previous_end, "@class.outer" },
+      }
+
+      for lhs, mapping in pairs(move_maps) do
+        vim.keymap.set({ "n", "x", "o" }, lhs, function()
+          mapping[1](mapping[2], "textobjects")
+        end)
+      end
 
       vim.keymap.set("n", "<leader>a", function()
         swap.swap_next("@parameter.inner")
@@ -129,14 +168,12 @@ return {
   },
   {
     "windwp/nvim-ts-autotag",
-    config = function()
-      require("nvim-ts-autotag").setup({
-        opts = {
-          enable_close = true,
-          enable_rename = true,
-          enable_close_on_slash = true,
-        },
-      })
-    end,
+    opts = {
+      opts = {
+        enable_close = true,
+        enable_rename = true,
+        enable_close_on_slash = true,
+      },
+    },
   },
 }
--- .config/nvim/lua/plugins/lang/tailwind.lua
+++ .config/nvim/lua/plugins/lang/tailwind.lua
@@ -1,14 +1,1 @@
-return {
-  "luckasRanarison/tailwind-tools.nvim",
-  name = "tailwind-tools",
-  build = ":UpdateRemotePlugins",
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-    "neovim/nvim-lspconfig",
-  },
-  opts = {
-    server = {
-      override = false,
-    },
-  },
-}
+return {}
