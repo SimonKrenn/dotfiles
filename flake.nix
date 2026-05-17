6cf95b734a440c7e87aac8e5a7d21bcdd226efbe
Simon Krenn <skrenn99@gmail.com>
2026-05-17 13:20:53 +0200

    chore: update nix flake

--- flake.nix
+++ flake.nix
@@ -1,40 +1,51 @@
 {
   description = "Nix dotfiles configuration";
   inputs = {
-      nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    
-      home-manager = {
-        url = "github:nix-community/home-manager/master";
-        inputs.nixpkgs.follows = "nixpkgs";
-      };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-      nix-darwin = {
-        url = "github:lnl7/nix-darwin/master";
-        inputs.nixpkgs.follows = "nixpkgs";
-      };
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-      nix-homebrew = {
-        url = "github:zhaofengli/nix-homebrew";
-      };
+    nix-darwin = {
+      url = "github:lnl7/nix-darwin/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-      devenv = {
-        url = "github:cachix/devenv";
-      };
+    nix-homebrew = {
+      url = "github:zhaofengli/nix-homebrew";
+    };
+
+    devenv = {
+      url = "github:cachix/devenv";
+    };
   };
 
-  outputs = {self, nixpkgs, nix-darwin, home-manager, nix-homebrew, devenv, ...}@inputs: {
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nix-darwin,
+      home-manager,
+      nix-homebrew,
+      devenv,
+      ...
+    }@inputs:
+    {
       darwinConfigurations."simon-mac" = nix-darwin.lib.darwinSystem {
         system = "aarch64-darwin";
         modules = [
           ./nix/darwin.nix
           ./nix/homebrew.nix
-          home-manager.darwinModules.home-manager {
+          home-manager.darwinModules.home-manager
+          {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.backupFileExtension = "stow-backup";
             home-manager.users.simonkrenn = import ./nix/home.nix;
-        }
-      ];
+          }
+        ];
+      };
     };
-  };
 }
